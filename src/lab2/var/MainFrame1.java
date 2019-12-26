1ac2103e9cdd6a53140f071c458c636e43776310
Nastya Leniuski <nastyamaseik@gmail.com>
2019-12-26 21:13:36 +0200

    Добавление calculate1 и calculate

--- src/lab2/var/MainFrame1.java
+++ src/lab2/var/MainFrame1.java
@@ -60,4 +60,35 @@ public class MainFrame1 extends JFrame {
     private Double mem2 = new Double(0);
     private Double mem3 = new Double(0);
 
+    //Формула №1 для рассчѐта
+    public Double calculate1(Double x, Double y, Double z) {
+
+        if (x == 0)	{
+            JOptionPane.showMessageDialog(MainFrame1.this,
+                    "x не может равняться нулю", "" +
+                            "Ошибка ввода", JOptionPane.WARNING_MESSAGE);
+            return 0.0;
+        }
+
+        if (y == 0)	{
+            JOptionPane.showMessageDialog(MainFrame1.this,
+                    "y не может равняться нулю", "" +
+                            "Ошибка ввода", JOptionPane.WARNING_MESSAGE);
+            return 0.0;
+        }
+
+        return Math.pow(Math.pow(Math.log(1+x),2) + Math.cos(Math.pow(Math.PI*z, 3)), Math.sin(y)) + Math.pow(Math.pow(Math.E, Math.pow(x, 2)) + Math.cos(Math.pow(Math.E,z)) + Math.pow(1/y, 1/2), 1/x);
+    }
+
+    //Формула №2 для рассчёта
+    public Double calculate2(Double x, Double y, Double z) {
+        if (x == 0)	{
+            JOptionPane.showMessageDialog(MainFrame1.this,
+                    "x не может равняться нулю", "" +
+                            "Ошибка ввода", JOptionPane.WARNING_MESSAGE);
+            return 0.0;
+        }
+
+        return Math.pow(Math.cos(Math.pow(Math.PI*x, 3)) + Math.pow(Math.log(1+y), 2), 4)*(Math.pow(Math.E, Math.pow(z, 2)) + Math.pow(1/x, 1/2) + Math.cos(Math.pow(Math.E, y)));
+    }
 }
